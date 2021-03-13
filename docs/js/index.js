226424ef4c12e062bace3c168ffe68ebb4227278
rbayarri <renzobayarri@gmail.com>
2021-03-13 19:03:20 -0300

    First commit

---
+++ docs/js/index.js
@@ -0,0 +1,9 @@
+function menu(){
+  var x = document.getElementById('menu-principal');
+  if (x.classList.contains("d-none-3")){
+    x.classList.remove("d-none-3");
+  }else{
+    x.classList.add("d-none-3");
+  }
+  
+}
